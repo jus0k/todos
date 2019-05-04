1296f534cf95f5919779c86111544761ff6e510a
jus0k <jutjd115@gmail.com>
2019-05-04 17:05:15 +0900

    Add First Upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Signin from './Signin';
+import Signup from './Signup';
+import Todos from './Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={Todos} />
+        <Route exact path="/signin" component={Signin} />
+        <Route exact path="/signup" component={Signup} />
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Todos.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+const url = 'http://13.113.246.46:8081';
+
+class Todos extends Component {
+  state = {
+    redirect: false,
+  }
+
+  componentDidMount() {
+    // checkLogin();
+  }
+
+  render() {
+    return (
+      <>
+      {!this.state.redirect && <Redirect to='/Signin' />}
+      <div>
+        정상적으로 로그인 되었습니다.
+      </div>
+      </>
+    );
+  }
+}
+
+export default Todos;
